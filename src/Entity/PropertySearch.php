7ce1309c2d1b8eb04c4adc09e82bd2748ba5d0b7
JJonathan <jolivald.jonathan@gmail.com>
2020-05-07 10:08:18 +0200

    options management

--- src/Entity/PropertySearch.php
+++ src/Entity/PropertySearch.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class PropertySearch {
@@ -17,6 +18,16 @@ class PropertySearch {
    */
   private $minSurface;
 
+  /**
+   * @var ArrayCollection
+   */
+  private $options;
+
+  
+  public function __construct()
+  {
+    $this->options = new ArrayCollection();
+  }
 
   /**
    * Get the value of maxPrice
@@ -66,4 +77,28 @@ class PropertySearch {
     return $this;
   }
 
+
+  /**
+   * Get the value of options
+   *
+   * @return  ArrayCollection
+   */ 
+  public function getOptions()
+  {
+    return $this->options;
+  }
+
+  /**
+   * Set the value of options
+   *
+   * @param  ArrayCollection  $options
+   *
+   * @return  self
+   */ 
+  public function setOptions(ArrayCollection $options)
+  {
+    $this->options = $options;
+
+    return $this;
+  }
 }
